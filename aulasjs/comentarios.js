df3d8ddee7cdc9e25a0c3f69d2227ba0aa2eea09
Patrícia Carvalho <patricia.calves111@gmail.com>
2022-12-27 16:58:37 -0300

    feat: adicionado aula sobre string

---
+++ aulasjs/comentarios.js
@@ -0,0 +1,8 @@
+// console.log("Bem vindos")
+// para comentar tem que usar duas barras 
+
+
+console.log("Bem vindo ao JS!")
+
+/* para mais de uma linha 
+de comentário */
---
+++ aulasjs/string.js
@@ -0,0 +1,26 @@
+/* 
+   *Cadeia de caracteres
+
+   "" - aspas duplas
+   '' - aspas simples
+   `` - template literals ou template strings
+
+    Se preciso usar aspas duplas ou simples dentro do código, então escolher uma para abrir e fechar sua string e ai pode usar a outra normal dentro exemplo
+
+    console.log("Patricia 'mora' no 'interior' ?")
+
+    Pois se usar o mesmo tipo de aspas, quebra o código e da erro. 
+
+    No caso das Crases, se eu inicio com ela, ela me da maior flexibilidade em pular linha e adicionar outras, diferentes das aspas duplas e simpples, como no exemplo:
+
+    console.log(`Patricia
+    mora
+    no
+    interior?`)
+
+    Além do mais, as crases nós permite colocar expressões de linguagens, como 1+1
+
+
+   */
+
+   console.log("Sempre usar um dos tipos citados a cima para escrever uma string")
