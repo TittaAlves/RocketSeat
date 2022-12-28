9b5923aedd747ad710e467cb8e1f7b93ab8f7d48
Patrícia Carvalho <patricia.calves111@gmail.com>
2022-12-28 08:25:20 -0300

    feat: adicionado aula sobre variáveis

---
+++ aulasjs/variaveis.js
@@ -0,0 +1,22 @@
+/*
+Variáveis
+
+- Nomes simbólicos para receber algum valor
+- Atalhos de código
+- Identificadores
+- 3 palavras reservadas para criar uma variável: var, let, const
+
+
+var e let da para mudar a variável a const não, quando atribuida ela não é mudada mais
+*/
+
+var clima = "Quente"           
+clima = "Frio"              //tanto var como let pode ser alterado a variável.
+
+console.log(clima)
+
+const tempo = "Tarde"        // const não pode ser alterado após ser nomeado 
+
+console.log(tempo)
+
+
