dc1aa6ea175e94d2dd678d7126e7d708145a2bfb
Patrícia Carvalho <patricia.calves111@gmail.com>
2022-12-29 21:35:37 -0300

    feat: adicionado aula sobre functions

---
+++ aulasjs/functions.js
@@ -0,0 +1,16 @@
+// criar um aplicativo de frases motivacionais
+
+
+// Passo 1: declaration - declaração da função - Function statement
+function createPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é a mão do aprendizado')
+}
+
+// executar a função
+// rodas, chamar, invocar
+// execute, run, call, invoke
+createPhrases()           // posso invocar a função mais de 1 x, quantas vezes quiser
+
+console.log('Fim do programa')
