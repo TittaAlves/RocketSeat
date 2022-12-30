1352b0ae1b4b29495fe0e30185af790d42252821
Patrícia Carvalho <patricia.calves111@gmail.com>
2022-12-29 21:42:28 -0300

    feat: adicionado aula de functions 1

---
+++ aulasjs/functions1.js
@@ -0,0 +1,11 @@
+
+// function expression
+//funcion anonymous
+
+
+//parâmetros(parameters)
+const sum = function(number1, number2) {
+    console.log(number1 + number2)
+}
+
+sum(2,3)   // arguments - argumentos
