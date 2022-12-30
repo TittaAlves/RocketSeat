7439120da52befb59f2453c875f1dfe5b42fab13
Patrícia Carvalho <patricia.calves111@gmail.com>
2022-12-29 22:13:21 -0300

    feat: adicionado mais atualizações da aula de functions

--- aulasjs/functions1.js
+++ aulasjs/functions1.js
@@ -1,4 +1,5 @@
-
+// -------------------------------------------------------------------
+// Aula Funções: Argumentos e parâmetros
 // // function expression
 // //funcion anonymous
 
@@ -10,15 +11,60 @@
 
 // sum(2,3)   // arguments - argumentos
 
+// -----------------------------------------------------------------
 
-const sum = function(number1, number2) {
-        let total = number1 + number2
-        return total
-}
+//aula retornando valores dentro da função
+// const sum = function(number1, number2) {
+//         let total = number1 + number2
+//         return total
+// }
 
-let number1 = 34
-let number2 = 25
+// let number1 = 34
+// let number2 = 25
 
-console.log(`O número 1 é ${number1}`)
-console.log(`O número 2 é ${number2}`)
-console.log(`a coma é ${sum(number1, number2)}`)
+// console.log(`O número 1 é ${number1}`)
+// console.log(`O número 2 é ${number2}`)
+// console.log(`a coma é ${sum(number1, number2)}`)
+
+// -----------------------------------------------------------------
+
+
+// aula outra maneira de entender funções
+// Função é um liquidificador
+
+// function fazerSuco(fruta1, fruta2) {
+//     return 'suco de: ' + fruta1 + fruta2
+// }
+
+// const copo = fazerSuco('banana', 'maça')
+
+// console.log(copo)
+
+//------------------------------------------------------------------
+
+//aula Function scope
+
+// let subject = 'creat video'
+
+// function createThink(subject) {
+//     subject = 'study'
+//     return subject
+// }
+
+// console.log(createThink(subject))
+// console.log(subject)
+
+
+// ---------------------------------------------------------------
+
+// //Aula Function Hoisting
+
+// sayMyName()
+
+
+// function sayMyName() {
+//     console.log('Patrícia')
+// }
+
+
+//----------------------------------------------------------------
