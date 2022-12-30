14bf6e92a2ff5b8e5a4708ded4768a73463797af
Patrícia Carvalho <patricia.calves111@gmail.com>
2022-12-29 21:52:43 -0300

    feat: adicionado continuação aula functions sobre retornando valores dentro da função

--- aulasjs/functions1.js
+++ aulasjs/functions1.js
@@ -1,11 +1,24 @@
 
-// function expression
-//funcion anonymous
+// // function expression
+// //funcion anonymous
 
 
-//parâmetros(parameters)
+// //parâmetros(parameters)
+// const sum = function(number1, number2) {
+//     console.log(number1 + number2)
+// }
+
+// sum(2,3)   // arguments - argumentos
+
+
 const sum = function(number1, number2) {
-    console.log(number1 + number2)
+        let total = number1 + number2
+        return total
 }
 
-sum(2,3)   // arguments - argumentos
+let number1 = 34
+let number2 = 25
+
+console.log(`O número 1 é ${number1}`)
+console.log(`O número 2 é ${number2}`)
+console.log(`a coma é ${sum(number1, number2)}`)
